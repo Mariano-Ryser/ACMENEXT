9a85bacb6634f77248ac20ca1311f057bf6cd114
MarianoRyser <rysermariano@gmail.com>
2024-11-30 20:13:26 +0100

    Todo ok probando

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -18,11 +18,11 @@ export default function Page() {
   className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
 />  
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Zeep.</strong>{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              
             </a>
-            , brought to you by Vercel.
+            
           </p>
           <Link
             href="/dashboard
@@ -34,7 +34,7 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-        <Image
+        {/* <Image
         src="/hero-desktop.png"
         width={1000}
         height={760}
@@ -47,7 +47,7 @@ export default function Page() {
         height={620}
         className="block md:hidden" //aqui se muestra en pantalla chica
         alt="Screenshot of the dashboard project showing mobile version"
-      />
+      /> */}
 
         </div>
       </div>
